3f60f42a793b81c4c54d3990bb66a4d12b00c46d
larsmostue <mostuelars@gmail.com>
2024-11-21 19:06:16 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hei, hva heter du?");
+            var input = Console.ReadLine();
+            Console.WriteLine($"Velkommen, {input}!");
+
+        }
+    }
+}
